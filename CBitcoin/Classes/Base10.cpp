0a5748e4833c9cc91eb8f7237a0627d3875913c2
Wolf McNally <wolf@wolfmcnally.com>
2018-10-25 12:10:21 -0700

    Added base 10.

---
+++ CBitcoin/Classes/Base10.cpp
@@ -0,0 +1,34 @@
+//
+//  Base10.cpp
+//  CBitcoin
+//
+//  Created by Wolf McNally on 10/24/18.
+//
+
+#include "Base10.hpp"
+#include <bitcoin/bitcoin.hpp>
+#include "Util.hpp"
+
+using namespace libbitcoin;
+
+uint8_t _btcDecimalPlaces() {
+    return btc_decimal_places;
+}
+
+uint8_t _mbtcDecimalPlaces() {
+    return mbtc_decimal_places;
+}
+
+uint8_t _ubtcDecimalPlaces() {
+    return ubtc_decimal_places;
+}
+
+void _encodeBase10(uint64_t amount, char** string, size_t* stringLength, uint8_t decimalPlaces) {
+    std::string s = encode_base10(amount, decimalPlaces);
+    _returnString(s, string, stringLength);
+}
+
+bool _decodeBase10(const char* string, uint64_t* amount, size_t decimalPlaces, bool strict) {
+    std::string s = std::string(string);
+    return decode_base10(*amount, s, static_cast<uint8_t>(decimalPlaces), strict);
+}
---
+++ CBitcoin/Classes/Base10.hpp
@@ -0,0 +1,28 @@
+//
+//  Base10.hpp
+//  CBitcoin
+//
+//  Created by Wolf McNally on 10/24/18.
+//
+
+#ifndef Base10_hpp
+#define Base10_hpp
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    uint8_t _btcDecimalPlaces();
+    uint8_t _mbtcDecimalPlaces();
+    uint8_t _ubtcDecimalPlaces();
+
+    void _encodeBase10(uint64_t amount, char** string, size_t* stringLength, uint8_t decimalPlaces);
+    bool _decodeBase10(const char* string, uint64_t* amount, size_t decimalPlaces, bool strict);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* Base10_hpp */
