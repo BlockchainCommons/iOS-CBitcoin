b320ef41136feb78474a8df6357da93a02b37232
Wolf McNally <wolf@wolfmcnally.com>
2019-02-21 01:52:52 -0800

    Integrating SSS (Shamir Secret Sharing.)

--- CBitcoin/Classes/SSS/shamir.c
+++ CBitcoin/Classes/SSS/shamir.c
@@ -14,3 +14,19 @@
 int _randombytes(void *buf, size_t n) {
     return randombytes(buf, n);
 }
+
+size_t _sss_message_length() {
+    return sss_MLEN;
+}
+
+size_t _sss_share_length() {
+    return sss_SHARE_LEN;
+}
+
+void _sss_create_shares(void *out, const uint8_t *data, uint8_t n, uint8_t k) {
+    sss_create_shares(out, data, n, k);
+}
+
+int _sss_combine_shares(uint8_t *data, const void *shares, uint8_t k) {
+    return sss_combine_shares(data, shares, k);
+}
--- CBitcoin/Classes/SSS/shamir.h
+++ CBitcoin/Classes/SSS/shamir.h
@@ -2,7 +2,13 @@
 #define shamir_h
 
 #include <stddef.h>
+#include <stdlib.h>
+
 
 int _randombytes(void *buf, size_t n);
+size_t _sss_message_length();
+size_t _sss_share_length();
+void _sss_create_shares(void *out, const uint8_t *data, uint8_t n, uint8_t k);
+int _sss_combine_shares(uint8_t *data, const void *shares, uint8_t k);
 
 #endif /* shamir_h */
